8fbe9960d60e3954cd4858f82e03b0299c1f37ff
skweel <dissingardjino@icloud.com>
2023-05-16 16:52:51 +0200

    mise en place

---
+++ lib/view/landing_page.dart
@@ -0,0 +1,111 @@
+import 'package:dots_indicator/dots_indicator.dart';
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+class LandinPageView extends StatefulWidget {
+  const LandinPageView({Key? key}) : super(key: key);
+
+  @override
+  State<LandinPageView> createState() => _LandinPageViewState();
+}
+
+class _LandinPageViewState extends State<LandinPageView> {
+  PageController pageController = PageController();
+  int positionDot = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: bodyPage(),
+
+    );
+  }
+
+  Widget bodyPage(){
+    return Column(
+        children: [
+          Flexible(
+            child: Container(
+              height: MediaQuery.of(context).size.height*0.5,
+              child: PageView(
+              controller: pageController,
+              onPageChanged: (value){
+                setState(() {
+                  positionDot = value;
+                });
+
+
+              },
+              children: [
+                Column(
+                  children: [
+                    Lottie.asset("assets/globe.json"),
+                    Text("mon texte")
+                  ],
+                ),
+
+                Column(
+                  children: [
+                    Lottie.asset("assets/relax.json"),
+                    Text("mon texte")
+                  ],
+                ),
+
+                Column(
+                  children: [
+                    Lottie.asset("assets/vacation.json"),
+                    Text("mon texte")
+                  ],
+                ),
+
+              ],
+
+    ),
+            ),
+          ),
+
+          //mes booutons et mon dot
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                  onPressed: (){
+                    setState(() {
+                      positionDot --;
+
+                      pageController.previousPage(duration: const Duration(milliseconds: 500), curve: Curves.linear);
+                    });
+                  },
+                  child: const Text("Précèdent")
+              ),
+              DotsIndicator(
+                position: positionDot,
+                  dotsCount: 3,
+
+              ),
+              ElevatedButton(onPressed: (){
+                if(positionDot == 2){
+                  //aller sur la nouvelle page
+                }
+                else {
+                  setState(() {
+                    positionDot ++;
+                    pageController.nextPage(
+                        duration: const Duration(milliseconds: 500),
+                        curve: Curves.linear);
+                  });
+                }
+
+
+              }, child: const Text("Suivant")
+              ),
+            ],
+          )
+
+        ],
+      );
+
+
+
+  }
+}
---
+++ lib/view/register_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RegitserView extends StatefulWidget {
+  const RegitserView({Key? key}) : super(key: key);
+
+  @override
+  State<RegitserView> createState() => _RegitserViewState();
+}
+
+class _RegitserViewState extends State<RegitserView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
