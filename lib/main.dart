79f80e4f45ad0842bd239959d6e2da5060c0c9dc
skweel <dissingardjino@icloud.com>
2023-05-30 14:59:18 +0200

    mise enplace des pubs

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,13 @@ import 'package:firstappesin/view/landing_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
