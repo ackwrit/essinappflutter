49babd3a19dc6367fbfc5dd8e73e6965823731b9
skweel <dissingardjino@icloud.com>
2023-06-13 09:44:24 +0200

    mise en place projet du firestoreHelper

---
+++ lib/controller/firestoreHelper.dart
@@ -0,0 +1,47 @@
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+
+class FirestoreHelper{
+  final auth = FirebaseAuth.instance;
+  final storage = FirebaseStorage.instance;
+  final cloudUsers= FirebaseFirestore.instance.collection("UTILISATEURS");
+  final cloudMessages = FirebaseFirestore.instance.collection("MESSAGES");
+
+  //méthode inscription
+  Future inscription(String prenom , String nom , String email , String password) async {
+    UserCredential resultat = await auth.createUserWithEmailAndPassword(email: email, password: password);
+    String uid =resultat.user!.uid;
+    Map<String,dynamic> data = {
+      "PRENOM":prenom,
+      "NOM":nom,
+      "PSEUDO":"",
+      "EMAIL":email
+
+    };
+    addUser(uid, data);
+
+
+  }
+
+
+  //méthode connexion
+
+
+  //methode mise à jour utilisateur
+  updateUser(String uid , Map<String,dynamic>data){
+    cloudUsers.doc(uid).update(data);
+
+  }
+
+  //suppression utilisateur
+
+  //creation utilisateur
+ addUser(String uid , Map<String,dynamic>data){
+    cloudUsers.doc(uid).set(data);
+
+ }
+
+
+}
