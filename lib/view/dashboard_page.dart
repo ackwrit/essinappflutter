ea59a5f5bb04c9adf375994c3563ec73d0f10d02
skweel <dissingardjino@icloud.com>
2023-06-13 11:59:13 +0200

    navigation bar

--- lib/view/dashboard_page.dart
+++ lib/view/dashboard_page.dart
@@ -1,3 +1,4 @@
+import 'package:firstappesin/view/background_view.dart';
 import 'package:flutter/material.dart';
 
 class DashBoard extends StatefulWidget {
@@ -8,8 +9,43 @@ class DashBoard extends StatefulWidget {
 }
 
 class _DashBoardState extends State<DashBoard> {
+  int index = 0;
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+      ),
+      extendBodyBehindAppBar: true,
+      body: bodyPage(),
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: (value){
+          setState(() {
+            index = value;
+          });
+        },
+        currentIndex: index,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.list),label: "Personnes"),
+          BottomNavigationBarItem(icon: Icon(Icons.map),label: "Carte"),
+          BottomNavigationBarItem(icon: Icon(Icons.favorite),label: "Favoris"),
+        ],
+
+      ) ,
+    );
+  }
+
+  Widget bodyPage(){
+    return Stack(
+      children: [
+        BackgroundView(),
+        PresentationView(),
+      ],
+    );
+  }
+
+  Widget PresentationView(){
+    return Text("couou");
   }
 }
